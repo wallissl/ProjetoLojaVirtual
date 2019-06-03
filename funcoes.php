c7bab532524b6db18ae0ad1de4bebcd521104c73
Wallis <wallis.sl@outlook.com>
2019-06-03 17:46:04 -0300

    Validando Formulario de compra

--- funcoes.php
+++ funcoes.php
@@ -12,3 +12,23 @@ $chave = count($produtos) + 1;
 $produtos["produto$chave"] = $novoProduto;
 return $produtos;
 }
+
+function validarNome($nome){
+    return strlen($nome) >= 3;
+}
+
+function validarCPF($cpf){
+    return strlen($cpf) == 11;
+}
+
+function validarCartao($nrnCartao){
+    return strlen($nrnCartao) == 16;
+}
+
+function validarDataValidade($dataValidade){
+    return $dataValidade > date('y-m-d');
+}
+
+function validarCVV($cvv) {
+    return strlen($cvv) == 3;
+}
--- sucesso.php
+++ sucesso.php
@@ -1,17 +1,33 @@
 <?php
 
-if($_POST){
-   
-
-  
 
 
-}else {
-    header("location:index.php");
+
+
+function validarCompra($dadosCompras){
+    $erros = [];
+    if(!$dadosCompras){
+        $erros[] = "Não foi recebido nenhum dado para realizar a compra!";
+
+    }
+    if(!validarNome($dadosCompras['nomeCliente'])){
+        $erros[] = "verifique se seu nome está correto, e se é maior que 2 caracteres!";
+    }
+    if(!validarCPF($dadosCompras['cpfCliente'])){
+        $erros[] = "CPF invalido amigão! tenta de novo!";
+    }
+    if(!validarCartao($dadosCompras['cartaoCliente'])){
+        $erros[] = "Dados da cartão invalido";
+    }
+    if(!validarCartao($dadosCompras['dataValidadeCartao'])){
+        $erros[] = "Validação invalida";
+    }
+    if(!validarCVV($dadosCompras['cvv'])){
+        $erros[] = "Data vencida";
+    }
+    
+    return $erros;
 }
-
-$nomeCompleto = $_POST['nomeCliente'];
-$nomeProduto = $_POST['nomeProduto'];
 
 ?>
 
@@ -29,12 +45,38 @@ $nomeProduto = $_POST['nomeProduto'];
 
     <main class="container">
         <section class="row">
+
+
+<?php
+
+        $errosValidacao = validarCompra($_POST);
+
+        if(count($errosValidacao) > 0):
+     ?>
+
+
+        <div class="col-md-12">
+        <!-- Aqui vai exibir erros de cadastro -->
+
+            <ul>
+                <?php foreach($errosValidacao as $erro): ?>
+                    <li><?php echo $erro; ?></li>
+                <?php endforeach; ?>
+
+            </ul>
+        
+        </div>
+
+
+
             <div class="col md-12">
             
             <div class="alert alert-success" role="alert">
-                Olá <?php echo $nomeCompleto; ?> Parabéns pela sua compra do produto <?php echo $nomeProduto; ?>
+                Olá <?php echo $_POST['nomeCliente']; ?> Parabéns pela sua compra do produto <?php echo $_POST['nomeProduto']; ?>
 
             </div>
+
+            <?php endif; ?>
             
             </div>
 
